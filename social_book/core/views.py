02b811c920b698542fb374220e1473509d541abe
shafinhabib <shafinhabib@gmail.com>
2023-11-27 21:46:37 +0800

    Fixed Login and Logout pages

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
+@login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
 
@@ -38,3 +40,27 @@ def signup(request):
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+    
+
+def login(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+    
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
