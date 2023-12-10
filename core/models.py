bff99a4ea914e4f922082529e1e082e9d4ddccf6
shafinhabib <shafinhabib@gmail.com>
2023-12-10 20:53:00 +0800

    Upload only text without images is now possible.

--- core/models.py
+++ core/models.py
@@ -19,7 +19,7 @@ class Profile(models.Model):
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='post_images', default=None)
+    image = models.ImageField(upload_to='post_images', blank=True, null=True, default='blank-profile-picture.png')
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
